2b77c958b22e58bd009498db451a05194c55ca84
Suranjan <suranjanbiswas2002@gmail.com>
2026-02-16 12:32:14 +0530

    add: added a variable example for the constraint system

--- src/r1cs.rs
+++ src/r1cs.rs
@@ -13,3 +13,33 @@ pub struct Variable{
   pub index: usize,
   pub value: BigInt
 }
+
+#[derive(Serialize, Deserialize)]
+pub enum Operation{
+  Add,
+  Mul,
+  Hash
+}
+
+/**
+ * Example
+ * 
+ * let x = Variable { index: 0, value: BigInt::from(5) };
+ * let y = Variable { index: 1, value: BigInt::from(7) };
+ * let z = Variable { index: 2, value: BigInt::from(12) };
+ * 
+ * let constraint = Constraint {
+ * left: vec![(x.clone(), x.value.clone())],
+ * right: vec![(y.clone(), y.value.clone())],
+ * output: vec![(z.clone(), z.value.clone())],
+ * operation: Operation::Add,
+ * };
+ */
+
+#[derive(Serialize, Deserialize)]
+pub struct Constraint{
+  pub left: Vec<(Variable, BigInt)>,
+  pub right: Vec<(Variable, BigInt)>,
+  pub output: Vec<(Variable, BigInt)>,
+  pub operation: Operation
+}
