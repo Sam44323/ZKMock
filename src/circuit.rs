befc7a850a3bac54d36e4cb6ae99c786c952d198
Suranjan <suranjanbiswas2002@gmail.com>
2026-02-16 12:14:17 +0530

    add: added the initial implementation of the circuit

--- src/circuit.rs
+++ src/circuit.rs
@@ -12,3 +12,35 @@ pub enum Gate{
   Mul(usize, usize, usize), // Mul: input-B, input-B, output
   Hash(usize, usize, usize) // Hash: input-A, input-B, output
 }
+
+pub struct Circuit{
+  hash_function: Option<Box<dyn HashFunction>>,
+  inputs: Vec<BigInt>,
+  gates: Vec<Gate>,
+  outputs: Vec<BigInt>
+}
+
+impl Circuit {
+  pub fn new(hash_function: Option<Box<dyn HashFunction>>) -> Self {
+    Circuit{
+      hash_function,
+      inputs: Vec::new(),
+      gates: Vec::new(),
+      outputs: Vec::new()
+    }
+  }
+
+  pub fn add_input(&mut self, input: BigInt) -> usize {
+    let index = self.inputs.len();
+    self.inputs.push(input);
+    index
+  }
+
+  pub fn add_gate(&mut self, gate: Gate) {
+    self.gates.push(gate);
+  }
+
+  pub fn add_output(&mut self, output: BigInt) {
+    self.outputs.push(output);
+  }
+}
