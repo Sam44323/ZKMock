7f0dbbfab48291fe09084e8fecf79402d0db0d7f
Suranjan <suranjanbiswas2002@gmail.com>
2026-02-16 14:13:26 +0530

    aadd: added the implementation of circuit-system

--- src/r1cs.rs
+++ src/r1cs.rs
@@ -39,3 +39,29 @@ pub struct Constraint{
   pub output: Vec<(Variable, BigInt)>,
   pub operation: Operation
 }
+
+#[derive(Serialize, Deserialize)]
+pub struct R1CS{
+  pub variables: Vec<Variable>,
+  pub constraints: Vec<Constraint>
+}
+
+impl R1CS {
+  pub fn new() -> Self {
+    R1CS{
+      variables: Vec::new(),
+      constraints: Vec::new()
+    }
+  }
+
+  pub fn add_constraints(&mut self, left: Vec<(Variable, BigInt)>, right: Vec<(Variable, BigInt)>, output: Vec<(Variable, BigInt)>, operation: Operation) {
+    let constraint = Constraint{
+      left,
+      right,
+      output,
+      operation
+    };
+    self.constraints.push(constraint);
+  }
+}
+
